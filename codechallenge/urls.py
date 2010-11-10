62035c34e5aa7f0c877abfca3d73dfafee32accf
nathanielksmith <nathanielksmith@gmail.com>
2010-11-09 22:30:42 -0500

    strip out some boilerplate

--- codechallenge/urls.py
+++ codechallenge/urls.py
@@ -1,20 +1,8 @@
 from django.conf.urls.defaults import *
 import tictactoe.views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Example:
-    # (r'^codechallenge/', include('codechallenge.foo.urls')),
     (r'^tictactoe/$', 'tictactoe.views.index'),
     (r'^tictactoe/board/(?P<board_id>\d+)$', 'tictactoe.views.index'),
     (r'^tictactoe/board/(?P<board_id>\d+)/move/(?P<row>\d+)/(?P<col>\d+)$', 'tictactoe.views.move'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
