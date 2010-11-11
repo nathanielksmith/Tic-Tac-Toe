8dd04a12df5cfd0ca9d10e0a6e483c70c7d2fb28
nathanielksmith <nathanielksmith@gmail.com>
2010-11-10 21:40:37 -0500

    implement the next move algorithm

--- codechallenge/tictactoe/models.py
+++ codechallenge/tictactoe/models.py
@@ -17,16 +17,49 @@ class GameState():
     def empty(self, row, col):
         return not bool(self.state[row][col])
 
+    def piece_at(self, row, col):
+        return self.state[row][col]
+
     def first_free(self):
         r,c = (0,0)
-        for row in self.state:
-            for col in row:
+        for r in range(0,2):
+            for c in range(0,2):
                 if self.empty(r,c):
                     return (r,c)
-                c += 1
-            r += 1
     
-    def two_in_a_row(self, piece):
+    def could_win(self, piece):
+        # inspired by http://stackoverflow.com/questions/1056316/algorithm-for-determining-tic-tac-toe-game-over-java/1056352#1056352
+        for r in range(0,3):
+            for c in range(0,3):
+                if self.piece_at(r,c) != piece:
+                    continue
+
+                # check diag
+                if r == c:
+                    has_piece = [(i,i) for i in range(0,3) if self.piece_at(i,i) == piece]
+                    is_empty  = [(i,i) for i in range(0,3) if self.empty(i,i)]
+                    if len(has_piece) == 2 and len(is_empty) == 1:
+                        return is_empty.pop()
+
+                # check antidiag
+                if (r,c) in [(0,2),(2,0),(1,1)]:
+                    has_piece = [(i,2-i) for i in range(0,3) if self.piece_at(i,2-i) == piece]
+                    is_empty  = [(i,2-i) for i in range(0,3) if self.empty(i,2-i)]
+                    if len(has_piece) == 2 and len(is_empty) == 1:
+                        return is_empty.pop()
+
+                # check row
+                has_piece = [(i,c) for i in range(0,3) if self.piece_at(i,c) == piece]
+                is_empty  = [(i,c) for i in range(0,3) if self.empty(i,c)]
+                if len(has_piece) == 2 and len(is_empty) == 1:
+                    return is_empty.pop()
+                    
+                # check col
+                has_piece = [(r,i) for i in range(0,3) if self.piece_at(r,i) == piece]
+                is_empty  = [(r,i) for i in range(0,3) if self.empty(r,i)]
+                if len(has_piece) == 2 and len(is_empty) == 1:
+                    return is_empty.pop()
+
         return (None, None)
 
 class Board(models.Model):
@@ -45,14 +78,14 @@ class Board(models.Model):
         self.state.update(user_row, user_col, 1)
 
         # Can I win?
-        row, col = self.state.two_in_a_row(2)
+        row, col = self.state.could_win(2)
 
         if row is not None and col is not None:
             self.state.update(row, col, 2)
             return
 
         # Are they about to win?
-        row, col = self.state.two_in_a_row(1)
+        row, col = self.state.could_win(1)
 
         if row is not None and col is not None:
             self.state.update(row, col, 2)
