88ea42bfad0312db351ce195a4587c28534d345b
nathanielksmith <nathanielksmith@gmail.com>
2010-11-10 20:12:43 -0500

    first pass at AI logic. Added some stubbed methods to GameState

--- codechallenge/tictactoe/models.py
+++ codechallenge/tictactoe/models.py
@@ -17,6 +17,18 @@ class GameState():
     def empty(self, row, col):
         return not bool(self.state[row][col])
 
+    def first_free(self):
+        r,c = (0,0)
+        for row in self.state:
+            for col in row:
+                if self.empty(r,c):
+                    return (r,c)
+                c += 1
+            r += 1
+    
+    def two_in_a_row(self, piece):
+        return (None, None)
+
 class Board(models.Model):
     state  = PickledObjectField(default=GameState())
     name   = models.CharField(default="a tic tac toe game", max_length=50) 
@@ -26,5 +38,31 @@ class Board(models.Model):
     def get_absolute_url(self):
         return r'/tictactoe/board/%i' % self.id
 
-    def user_move(self, row, col):
-        self.state.update(row, col, 1)
+    def user_move(self, user_row, user_col):
+        if not self.state.empty(user_row,user_col):
+            return
+
+        self.state.update(user_row, user_col, 1)
+
+        # Can I win?
+        row, col = self.state.two_in_a_row(2)
+
+        if row is not None and col is not None:
+            self.state.update(row, col, 2)
+            return
+
+        # Are they about to win?
+        row, col = self.state.two_in_a_row(1)
+
+        if row is not None and col is not None:
+            self.state.update(row, col, 2)
+            return
+
+        # Is the center free?
+        if self.state.empty(self.state.center_row(), self.state.center_col()):
+            self.state.update(self.state.center_row(), self.state.center_col(), 2)
+            return
+
+        # Fall back to arbitrary
+        row, col = self.state.first_free()
+        self.state.update(row,col, 2)
